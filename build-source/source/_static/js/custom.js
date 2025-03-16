d539912e28ae3b6c31be11fdcf65da37056baa46
Yang Huang <hymath@gmail.com>
2025-03-16 18:51:41 +0100

    done refactoring weinbergs qft

--- build-source/source/_static/js/custom.js
+++ build-source/source/_static/js/custom.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('.reference.internal[href*="#equation-"]');
     links.forEach(function(link) {
+        // Create a div to be inserted under the equation link
         const tooltip = document.createElement('div');
         tooltip.className = 'eq-tooltip';
         link.classList.add('equation-link');
 
-        const eqUrl = link.getAttribute('href');
-        const eqId = eqUrl.split('#')[1];
-        fetch(eqUrl)
+        const eqnUrl = link.getAttribute('href');
+        const eqnId = eqnUrl.split('#')[1];
+        fetch(eqnUrl)
             .then(response => response.text())
             .then(html => {
                 const parser = new DOMParser();
                 const doc = parser.parseFromString(html, 'text/html');
-                const eqElem = doc.getElementById(eqId);
-                if (eqElem) {
-                    // Set eqno invisible if there is any
-                    let eqnoElem = eqElem.querySelector('.eqno');
+                const eqnElem = doc.getElementById(eqnId);
+                if (eqnElem) {
+                    // Set `eqno` invisible if there is any
+                    const eqnoElem = eqnElem.querySelector('.eqno');
                     if (eqnoElem) {
                         eqnoElem.style.display = 'none';
                     }
 
-                    tooltip.innerHTML = eqElem.innerHTML;
+                    tooltip.innerHTML = eqnElem.innerHTML;
 
                     if (typeof MathJax !== 'undefined') {
                         MathJax.typeset([tooltip]);
